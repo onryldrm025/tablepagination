98947b24884f639f1dda1a97c64dfce89f240cbd
Onur Yıldırım <onryldrm025@hotmail.com>
2021-10-18 22:26:21 +0300

    table pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+
+let data = [
+  {
+    id: "1",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "2",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "3",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "4",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "5",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "6",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "7",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "8",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "9",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "10",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "11",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "12",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "13",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "14",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "15",
+    name: "onur",
+    surname: "yıldırım",
+  },
+  {
+    id: "16",
+    name: "onur",
+    surname: "yıldırım",
+  },
+];
 
 function App() {
+ 
+
+  const [page_number,setpage] = useState(0);
+
+  const page_size = 5;
+
+  var partdata = data.slice(
+    page_number * page_size,
+    page_number * page_size + page_size
+  );
+
+  const [mydata, setData] = useState(partdata);
+
+  const pages = [...Array(Math.ceil(data.length / page_size)).keys()];
+
+  const changePage = (page_number) => {
+    var datas = data.slice(
+      page_number * page_size,
+      page_number * page_size + page_size
+    );
+    setData(datas);
+    setpage(page_number)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>surname</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mydata.map((e, index) => (
+            <tr key={index}>
+              <td>{e.id}</td>
+              <td>{e.name}</td>
+              <td>{e.surname}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="pages">
+        {pages.map((e, index) => (
+          <div
+            onClick={() => {
+              changePage(e);
+            }}
+            className={`pages-item ${page_number === index ? 'active':''}`}
+            key={index}
+          >
+            {e + 1}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
